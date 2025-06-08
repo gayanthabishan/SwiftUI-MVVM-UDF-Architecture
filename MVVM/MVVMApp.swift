d01492f4e4c7545e7061cfb1b436bb00b6afe013
Bishan Meddegoda <bishan.meddegoda@pickme.lk>
2025-06-08 17:13:23 +0530

    Initial Commit

---
+++ MVVM/MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMApp.swift
+//  MVVM
+//
+//  Created by Bishanm on 2025-06-08.
+//
+
+import SwiftUI
+
+@main
+struct MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MVVMTests/MVVMTests.swift
@@ -0,0 +1,16 @@
+//
+//  MVVMTests.swift
+//  MVVMTests
+//
+//  Created by Bishanm on 2025-06-08.
+//
+
+import Testing
+
+struct MVVMTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
